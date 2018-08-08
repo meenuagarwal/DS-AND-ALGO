8b9975b8577bcc08ce61505fdb8df15e2ad433da
Meenu  Agarwal <mee.agarwal97@gmail.com>
2018-08-08 23:51:59 +0530

    Add files via upload

---
+++ Stacks and Queues/check_brackets.py
@@ -0,0 +1,51 @@
+# python2
+
+import sys
+
+class Bracket:
+    def __init__(self, bracket_type, position):
+        self.bracket_type = bracket_type
+        self.position = position
+
+    def Match(self, c):
+        if self.bracket_type == '[' and c == ']':
+            return True
+        if self.bracket_type == '{' and c == '}':
+            return True
+        if self.bracket_type == '(' and c == ')':
+            return True
+        return False
+
+if __name__ == "__main__":
+    text = raw_input()
+    res = "True"
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next == '(' or next == '[' or next == '{':
+            # Process opening bracket, write your code here
+            opening_brackets_stack.append(Bracket(next,i))
+
+        if next == ')' or next == ']' or next == '}':
+            # Process closing bracket, write your code here
+            if (not opening_brackets_stack):
+                print (i+1)
+                res = "False"
+                break
+            else:
+                popped = opening_brackets_stack.pop()
+                if (popped.Match(next) == False):
+                    print (i+1)
+                    res = "False"
+                    break
+
+    if (res == "True"):
+        if (len(opening_brackets_stack) == 0):
+            print ("Success")
+        else:
+            unmatched = opening_brackets_stack.pop()
+            print (unmatched.position + 1)
+
+
+
+
+    # Printing answer, write your code here
